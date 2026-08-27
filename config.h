94703af2ef1dd628d5295ebe4268be32f69b0b75
Sina <sinagessner.wuppertal@gmail.com>
2026-08-27 15:49:20 +0200

    Simplify RGB defaults and bump VIA lighting UI version

--- config.h
+++ config.h
@@ -4,7 +4,6 @@
 
 // RGB Matrix defaults. WS2812 pin and LED layout live in keyboard.json.
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120
-#define RGB_MATRIX_DEFAULT_ON true
 #define RGB_MATRIX_DEFAULT_HUE 149
 #define RGB_MATRIX_DEFAULT_SAT 255
 #define RGB_MATRIX_DEFAULT_VAL 40
@@ -13,7 +12,7 @@
 // VIA custom layer-lighting UI + persistent settings.
 // Each of the five layers stores two independently selectable colors,
 // an effect and its speed.
-#define VIA_FIRMWARE_VERSION 4
+#define VIA_FIRMWARE_VERSION 5
 #define EECONFIG_USER_DATA_SIZE 32
 #define EECONFIG_USER_DATA_VERSION 4
 
