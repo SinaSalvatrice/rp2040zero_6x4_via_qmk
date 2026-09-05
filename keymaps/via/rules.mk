89824c05661453fac0350b3d05765fd16d9ecb04
Sina <sinagessner.wuppertal@gmail.com>
2026-09-06 00:32:32 +0200

    Limit tap dance override include to C sources

--- keymaps/via/rules.mk
+++ keymaps/via/rules.mk
@@ -1,4 +1,4 @@
 VIA_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 SRC += tap_dance_override.c
-OPT_DEFS += -include $(KEYMAP_PATH)/tap_dance_override.h
+CFLAGS += -include $(KEYMAP_PATH)/tap_dance_override.h
